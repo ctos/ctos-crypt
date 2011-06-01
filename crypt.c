c5e5bf031c7d05ed375ab8fd77832d2dca3c7aea
Sharlene <shachulin@gmail.com>
2011-06-01 11:01:19 +0800

    basic function is finished

--- crypt.c
+++ crypt.c
@@ -2,8 +2,15 @@
 #include <mcrypt.h>
 #include <stdio.h>
 #include <stdlib.h>
-/* #include <mhash.h> */
+#define algorithm "twofish"
+#define mode  "cfb"
+#define keysize 16
+#define saltsize 20
+#define ivsize  16
 
+char* crypt_malloc(size_t size);
+void crypt_random(char *buf, size_t size);
+void putin_meg(char *ciphertext, char *salt, char *IV);
 int encrypt(char *password, char *plaintext, char *ciphertext)
 {
 
@@ -12,33 +19,24 @@ int encrypt(char *password, char *plaintext, char *ciphertext)
 	char *key;
 	char block_buffer;
 	char *IV;
-	int keysize=16; /* 128 bits */
+	char *salt;
 	
-	key=calloc(1, keysize);
-
-/* Generate the key using the password */
-/* mhash_keygen( KEYGEN_MCRYPT, MHASH_MD5, key, keysize, NULL, 0, password, strlen(password));
-*/
-	memmove( key, password, strlen(password));
-	
-	td = mcrypt_module_open("twofish", NULL, "cfb", NULL);
-	if (td==MCRYPT_FAILED)
+	td = mcrypt_module_open(algorithm, NULL, mode, NULL);
+	if (td == MCRYPT_FAILED)
 	{
+		printf("failed to open module\n");		
 		return 1;
 	}
-        IV = malloc(mcrypt_enc_get_iv_size(td));
-       /* Put random data in IV. Note these are not real random data,
-        * consider using /dev/random or /dev/urandom.
-        */
 
-         /*  srand(time(0)); */
+	key = crypt_malloc(keysize);
+	salt = crypt_malloc(saltsize);
+	crypt_random(salt, saltsize);
+        IV = crypt_malloc(ivsize);
+	crypt_random(IV, ivsize);
 
-	for (i=0; i< mcrypt_enc_get_iv_size( td); i++)
-	{
-        	IV[i]=rand();
-        }
-
-        i=mcrypt_generic_init( td, key, keysize, IV);
+	putin_meg(ciphertext, salt, IV);
+	
+	i = mcrypt_generic_init( td, key, keysize, IV);
         if (i<0) 
 	{
         	mcrypt_perror(i);
@@ -46,15 +44,13 @@ int encrypt(char *password, char *plaintext, char *ciphertext)
         }
 //	printf("%d",strlen(plaintext));
          /* Encryption in CFB is performed in bytes */
-        for(i=0;i<=strlen(plaintext);i++)
+        for(i=36; i<strlen(plaintext); i++)
 	{
 //		printf("%c",plaintext[i]);
-		block_buffer=plaintext[i];
+		block_buffer = plaintext[i];
         	mcrypt_generic (td, &block_buffer, 1);
-        	ciphertext[i]=block_buffer; 
+        	ciphertext[i] = block_buffer; 
 
-       /* Comment above and uncomment this to decrypt */
-       /*    mdecrypt_generic (td, &block_buffer, 1);  */
 //		printf("%c",ciphertext[i]);
 	}
 
@@ -63,7 +59,58 @@ int encrypt(char *password, char *plaintext, char *ciphertext)
 
          return 0;
 
-       }
+}
+
+char *crypt_malloc(size_t size)
+{
+	char *x;
+	
+	if(size==0)
+		return NULL;
+	x = malloc(size);
+	if(x!=NULL)
+		return x;
+	else
+	{
+		printf("Can't allocate memory!\n");
+		exit(-1);
+	}
+}
+	
+void crypt_random(char *buf, size_t size)
+{
+	int i;
+	
+	srand(time(0));
+	for(i=0;i<size;i++)
+	{
+		buf[i]=rand();
+	}
+}
+
+void putin_meg(char *ciphertext, char *salt, char *IV)
+{
+	
+	strcpy(ciphertext, salt);
+	strcat(ciphertext, IV);	
+}
+
+void read_meg(char *ciphertext, char *salt, char *IV)
+{
+	int i;
+	
+	for (i=0; i< saltsize; i++)
+	{
+        	salt[i] = ciphertext[i];
+        }
+	
+	for(i=saltsize; i<ivsize; i++)
+	{
+		IV[i] = ciphertext[i];
+	}
+	
+}
+
 int dencrypt(char *password, char *ciphertext, char *plaintext)
 {
 
@@ -72,31 +119,18 @@ int dencrypt(char *password, char *ciphertext, char *plaintext)
 	char *key;
 	char block_buffer;
 	char *IV;
-	int keysize=16; /* 128 bits */
-	
-	key=calloc(1, keysize);
-
-/* Generate the key using the password */
-/* mhash_keygen( KEYGEN_MCRYPT, MHASH_MD5, key, keysize, NULL, 0, password, strlen(password));
-*/
-	memmove( key, password, strlen(password));
+	char *salt;	
 	
 	td = mcrypt_module_open("twofish", NULL, "cfb", NULL);
 	if (td==MCRYPT_FAILED)
 	{
 		return 1;
 	}
-        IV = malloc(mcrypt_enc_get_iv_size(td));
-       /* Put random data in IV. Note these are not real random data,
-        * consider using /dev/random or /dev/urandom.
-        */
 
-         /*  srand(time(0)); */
+	salt = crypt_malloc(saltsize);
+        IV = crypt_malloc(ivsize);
 
-	for (i=0; i< mcrypt_enc_get_iv_size( td); i++)
-	{
-        	IV[i]=rand();
-        }
+	read_meg(ciphertext, salt, IV);
 
         i=mcrypt_generic_init( td, key, keysize, IV);
         if (i<0) 
@@ -106,7 +140,7 @@ int dencrypt(char *password, char *ciphertext, char *plaintext)
         }
 //	printf("%d",strlen(plaintext));
          /* Encryption in CFB is performed in bytes */
-        for(i=0;i<=strlen(ciphertext);i++)
+        for(i=saltsize + ivsize; i<=strlen(ciphertext); i++)
 	{
 //		printf("%c",plaintext[i]);
 		block_buffer=ciphertext[i];
